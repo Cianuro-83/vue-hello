2ea8316204e6431752fc3b8c06053624d9991755
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-17 16:09:24 +0100

    FEAT: creazione cartella bonus e upload img

---
+++ BONUS/js/scb.js
@@ -0,0 +1,23 @@
+console.log("Ciao Cianuro... Oggi l'esercizio è  VUE HELLO");
+
+//---------------------------------------------------------------------------
+// ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇ VUE ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
+//---------------------------------------------------------------------------
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: "ciao simone cianuro buzzeo!!!",
+      classMessage: "text-center text-uppercase text-bg-warning fw-bolder fs-1",
+      descrizione:
+        "oggi è la prima volta che metti le mani su VUE... cerca di non fare danni come al tuo solito...",
+      classDescrizione:
+        "text-center my-5 text-success fs-3 text-decoration-underline",
+    };
+  },
+}).mount("#app");
+
+//---------------------------------------------------------------------------
+// ⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆ VUE ⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
+//---------------------------------------------------------------------------
